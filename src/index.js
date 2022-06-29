fe22eb8d6b83153af4643b4b64b0df0c31eed816
Johnny01232003 <89808275+Johnny01232003@users.noreply.github.com>
2022-06-28 19:55:57 -0700

    Onboarding commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 
 app.get('/', (req, res ) => 
-    res.json({ message: 'Docker is easy 🐳' }) 
+    res.json({ message: 'The walkthrough was very informative' }) 
 );
 
 const port = process.env.PORT || 8080;
